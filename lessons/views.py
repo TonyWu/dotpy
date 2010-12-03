c93272e879d7f1e0dd2bab2ab393c4e7e514600c
xhh <xhh@xhh.me>
2010-12-03 22:14:13 +0800

    Create cache files for lessons by processing with Markdown

--- lessons/views.py
+++ lessons/views.py
@@ -23,6 +23,33 @@ def _load_with_slug(slug):
     raise Http404()
   return obj
 
+def _check_markdown_cache(lesson):
+  '''
+  Check if the cache file exists for this lesson.
+  If not, create one with the results of Markdown processing.
+  The "cache file" means the resulting HTML content of the
+  Markdown source of the lesson.
+  '''
+  import os
+  from os import path
+  base_dir = path.abspath(path.dirname(__file__))
+  # first, check the cache directory
+  cache_dir = path.join(base_dir, 'templates/cache/')
+  if not path.exists(cache_dir):
+    os.mkdir(cache_dir)
+  markdown_template = '%s.htm' % lesson.slug
+  cache_file = path.join(cache_dir, markdown_template)
+  # now check the cache file, if not exist, create it
+  if not path.exists(cache_file):
+    import markdown
+    import codecs
+    cache_content = markdown.markdown(lesson.content)
+    cache_output = codecs.open(cache_file, 'wU', 'utf-8')
+    cache_output.write(cache_content)
+  # now the HTML file should exist
+  markdown_template = 'cache/%s' % markdown_template
+  return markdown_template
+
 def show(request, lesson_slug):
   if not lesson_slug:
     # show lessons home page
@@ -31,8 +58,13 @@ def show(request, lesson_slug):
   # load lesson from database
   lesson = _load_with_slug(lesson_slug)
 
+  # check if the cache file exists for this lesson
+  markdown_template = _check_markdown_cache(lesson)
   return render_to_response('lesson.htm', \
-            {'lesson':lesson, 'comments_num': lesson.comment_set.count()})
+            {'lesson':lesson, \
+             'comments_num': lesson.comment_set.count(), \
+             'markdown_template': markdown_template
+            })
 
 def show_comments(request, lesson_slug):
   lesson = _load_with_slug(lesson_slug)
